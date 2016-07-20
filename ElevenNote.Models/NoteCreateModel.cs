beb9a0726ed95ad0b1c79089c7d5c14a8e20ee64
Ron <rjschnell60@outlook.com>
2016-07-20 11:55:18 -0400

    Added some files to the model

---
+++ ElevenNote.Models/NoteCreateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreateModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return $"[New] {Title}";
+        }
+    }
+}
---
+++ ElevenNote.Models/NoteListItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItemModel
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public override string ToString()
+        {
+            return $"[{NoteId}] {Title}";
+        }
+    }
+}
